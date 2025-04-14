94b456ab9d2aa43a45e45e691165aa8b49408d5a
Milos Ilic <milosilic89@outlook.com>
2025-04-14 03:54:17 +0200

    fix: Lint errors

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: { name, email, password: hashedPassword },
   });
 
